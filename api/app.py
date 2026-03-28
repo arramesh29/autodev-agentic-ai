c83e49bf8d1c21278bbdd55f36fdc35c750325d2
arramesh29 <rameshprabha23@gmail.com>
2026-03-28 10:40:53 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from workflows.development_workflow import run_workflow
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
