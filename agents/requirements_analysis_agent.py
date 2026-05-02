468adf351ddf0edef952a5b2f007656b1d7326dd
arramesh29 <rameshprabha23@gmail.com>
2026-05-02 10:07:54 +0530

    Update JSOn format

--- agents/requirements_analysis_agent.py
+++ agents/requirements_analysis_agent.py
@@ -4,13 +4,13 @@ from services.llm_service import llm
 def analyze_requirements(raw_requirement):
 
     prompt = f"""
-    Analyze automotive requirement and return STRICT JSON:
+    Analyze automotive requirement and return STRICT JSON.
 
     Tasks:
     - Split into atomic requirements
     - Assign ID: REQ-XXX
     - Mark testable = true
-    - Tag: functional / safety / performance
+    - Tag: functional / safety / performance / Non functional / No requirement
     - Detect conflicts
     - Detect ambiguities
 
@@ -18,31 +18,31 @@ def analyze_requirements(raw_requirement):
     {raw_requirement}
 
     JSON format example:
-	{
-  "requirements": [
-    {
-      "id": "REQ-001",
-      "description": "Compute time-to-collision",
-      "type": "functional",
-      "priority": "high",
-      "atomic": True,
-      "testable": True,
-      "tags": ["AEB", "safety"]
-    }
-  ],
-  "conflicts": [
-    {
-      "req_ids": ["REQ-002", "REQ-005"],
-      "reason": "Conflicting braking thresholds"
-    }
-  ],
-  "ambiguities": [
-    {
-      "req_id": "REQ-003",
-      "issue": "Speed range not defined"
-    }
-  ]
-}
+		{
+		"requirements": [
+		    {
+		      "id": "REQ-001",
+		      "description": "Compute time-to-collision",
+		      "type": "functional",
+		      "priority": "high",
+		      "atomic": True,
+		      "testable": True,
+		      "tags": ["AEB", "safety"]
+		    }
+		  ],
+		  "conflicts": [
+		    {
+		      "req_ids": ["REQ-002", "REQ-005"],
+		      "reason": "Conflicting braking thresholds"
+		    }
+		  ],
+		  "ambiguities": [
+		    {
+		      "req_id": "REQ-003",
+		      "issue": "Speed range not defined"
+		    }
+		  ]
+		}
 	
     """
 
