637cfee045b85c5842f83ced954b03a2ab8322e1
arramesh29 <rameshprabha23@gmail.com>
2026-05-23 12:15:05 +0530

    Update rag_orchestrator.py

--- tools/rag/rag_orchestrator.py
+++ tools/rag/rag_orchestrator.py
@@ -1,13 +1,10 @@
 from tools.rag.hybrid_retriever import hybrid_search
-from tools.rag.reranker import rerank
 from tools.rag.context_builder import build_context
 
 
 def retrieve_context(query, agent_type):
 
     docs = hybrid_search(query)
-
-#    docs = rerank(query, docs)
 
     context = build_context(docs)
 
